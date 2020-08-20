dd69373488f2bb0bc44561fcfe78586569e532fd
Konrad <konrad.gmur@gmail.com>
2020-08-20 19:33:59 +0200

    update project

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    servers: [],
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
+    return <div className="App">App</div>;
   }
 }
 
