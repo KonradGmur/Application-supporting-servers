abb008febd68481b689e01dce0f61643d3198d90
Konrad <konrad.gmur@gmail.com>
2020-08-23 20:04:03 +0200

    Update Servers.js

--- src/components/Servers.js
+++ src/components/Servers.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Servers.css";
+import Button from "./button";
 
 const Servers = (props) => (
   <ul>
     <li>
       <strong>{props.name} </strong>
       <strong>{props.status}</strong>
+      <Button />
     </li>
   </ul>
 );
