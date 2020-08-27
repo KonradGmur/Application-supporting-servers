d48855749794a54665f9998f128dffbeef3312a3
Konrad <konrad.gmur@gmail.com>
2020-08-27 22:19:06 +0200

    Change button in Servers.js

--- src/components/Servers.js
+++ src/components/Servers.js
@@ -8,7 +8,12 @@ const Servers = (props) => (
       <span className="status2">{props.name}</span>
       <span className="status">&bull; {props.status}</span>
       <span className="status3"></span>
-      <DropdownButton btnSize="xs" btnStyle="link" onSelect={(eventKey) => {}}>
+      <DropdownButton
+        btnSize="xs"
+        btnStyle="link"
+        title="..."
+        onSelect={(eventKey) => {}}
+      >
         <MenuItem eventKey={1}>Turn off</MenuItem>
         <MenuItem eventKey={2}>Reboot</MenuItem>
       </DropdownButton>
